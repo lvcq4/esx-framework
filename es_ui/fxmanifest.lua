1cf15c9a8f6622b6472028ccfdfcf673b093d4e2
lvcq4 <luca@razuuu.de>
2023-10-28 02:01:05 +0200

    uploaded files

---
+++ es_ui/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'es_ui by Kanersps.'
+
+ui_page 'ui.html'
+
+client_script 'client.lua'
+
+-- NUI Files
+files {
+	'ui.html',
+	'pdown.ttf'
+}
+
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
---
+++ esplugin_mysql/__resource.lua
@@ -0,0 +1,16 @@
+server_script '@mysql-async/lib/MySQL.lua'
+server_script 'server.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
+
+client_script '@waveshield-ac/xDxDxDxDxD.lua'
